59290fe48b9038793809ee5fe02a0d4ae82885ca
Alexey Kundro <exeykundro@gmail.com>
2022-08-24 03:06:20 +0300

    ref: code refactoring

--- ZooLab/tests/ZooLab.Tests/ValidatorsTests/ZookeeperHireValidatorTests.cs
+++ ZooLab/tests/ZooLab.Tests/ValidatorsTests/ZookeeperHireValidatorTests.cs
@@ -1,6 +1,34 @@
-﻿namespace ZooLab.Tests.ValidatorsTests
+﻿using FluentAssertions;
+using Xunit;
+using ZooLab.Animals;
+using ZooLab.Animals.Bird;
+using ZooLab.Animals.Mammal;
+using ZooLab.Employees;
+using ZooLab.Exceptions;
+using ZooLab.Validators;
+
+namespace ZooLab.Tests.ValidatorsTests
 {
     public class ZookeeperHireValidatorTests
     {
+        [Fact]
+        public void ShouldBeAbleToCreateValidator()
+        {
+            ZookeeperHireValidatorTests validator = new ZookeeperHireValidatorTests();
+            validator.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void ShouldBeAbleToValidateZookeeper()
+        {
+            ZookeeperHireValidator validator = new ZookeeperHireValidator();
+            Zoo zoo = new Zoo("zoo1");
+            zoo.AddEnclosure("enclosure1", 1000, zoo);
+            Parrot parrot = new Parrot();
+            zoo.AddAnimal(parrot);
+            Zookeeper employee = new Zookeeper("name", "surname");
+            employee.AddAnimalExperience(parrot);
+            validator.ValidateEmployee(employee, zoo);
+        }
     }
 }
